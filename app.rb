dee5e23ab92eeaceb37f9d26e73fd1b99e2c6712
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-23 07:41:14 +0000

    Add app.rb, views and Battle index

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+require_relative './lib/player'
+require_relative './lib/game'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb(:index)
+  end
+
+  #Start the server if ruby file executed directly
+  run! if app_file = $0
+end
